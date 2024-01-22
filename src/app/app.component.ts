3920c5222c55276ea6ce0f692c9fa35664e1e802
Michael Burger <michael.burger@orma-solutions.it>
2024-01-22 16:41:09 +0100

    unable to resolve crypto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import eccrypto from "@toruslabs/eccrypto";
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ng-crypto-esbuild';
+
+  constructor() {
+    console.log(eccrypto);
+  }
 }
